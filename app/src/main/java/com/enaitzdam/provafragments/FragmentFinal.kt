e012cc9625cdd7c21d38c470531de97bf935b88b
Enaitz <eechagui@xtec.cat>
2024-11-25 12:46:48 +0100

    Bottom

--- app/src/main/java/com/enaitzdam/provafragments/FragmentFinal.kt
+++ app/src/main/java/com/enaitzdam/provafragments/FragmentFinal.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.enaitzdam.provafragments.databinding.FragmentFinalBinding
+import com.enaitzdam.provafragments.databinding.FragmentIniciBinding
 
 
 class FragmentFinal : Fragment() {
@@ -15,6 +18,12 @@ class FragmentFinal : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_final, container, false)
+        val binding = FragmentFinalBinding.inflate(inflater)
+
+        binding.buttonGo?.setOnClickListener {
+            findNavController().navigate(R.id.action_fragmentFinal_to_boatFragment, null)
+        }
+
+        return binding.root
     }
 }
--- app/src/main/java/com/enaitzdam/provafragments/MainActivity.kt
+++ app/src/main/java/com/enaitzdam/provafragments/MainActivity.kt
@@ -2,28 +2,86 @@ package com.enaitzdam.provafragments
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
 import androidx.navigation.ui.NavigationUI.setupWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var toolbar:Toolbar
+    private lateinit var appBarConfig: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
+
+        //BOTTOM NAVIGATION
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
         navController = navHostFragment.navController
 
         val bottomNavView = findViewById<BottomNavigationView>(R.id.bottomNAvigationView)
 
-        setupWithNavController(bottomNavView, navController )
+        bottomNavView.setupWithNavController(navController)
+
+
+
+        //NAVIGATION DRAWER BASIC
+        val navView = findViewById<NavigationView>(R.id.nav_view)
+        navView.setupWithNavController(navController)
+
+
+
+
+
+        //NAVIGATION DRAWER AMB ACTIONBAR
+        drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
+        toolbar=findViewById(R.id.toolbar)
+
+
+        //The given Activity will be linked to the specified DrawerLayout and
+        // its Actionbar's Up button will be set to a custom drawable.
+        //This drawable shows a Hamburger icon when drawer is closed and an arrow when drawer is open.
+        // It animates between these two states as the drawer opens.
+        val toggle = ActionBarDrawerToggle(
+            this, drawerLayout, R.string.nav_open, R.string.nav_close
+        )
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        appBarConfig = AppBarConfiguration.Builder(R.id.fragmentInici)
+        //appBarConfig = AppBarConfiguration.Builder(R.id.fragmentInici, R.id.fragmentFinal, R.id.boatFragment)
+            .setDrawerLayout(drawerLayout)
+            .build()
+
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+
+        setupActionBarWithNavController(this, navController, appBarConfig)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        // replace navigation up button with nav drawer button when on start destination
+        return NavigationUI.navigateUp(navController, appBarConfig)
+
     }
 }
